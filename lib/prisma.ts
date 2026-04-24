784a0f8524a1aba3fe0c7721172d5e48a881dd57
Ammad <ar.dev.tech47@gmail.com>
2026-04-24 16:56:15 +0500

    last commit

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,10 @@
 import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+  prisma?: PrismaClient
 }
 
-export const prisma =
+export const prisma: PrismaClient =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: ['error'],
